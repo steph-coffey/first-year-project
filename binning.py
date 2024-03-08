057ad75156a00eafe95e2b7a2380b716e391aa2a
Stephen Coffey <coffey@umbriel.astro.columbia.edu>
2024-03-08 09:59:33 -0500

    rewrote binning function a bit, cleaned up, and compared white light curves

--- binning.py
+++ binning.py
@@ -1,5 +1,30 @@
 import numpy as np
 
+def white_light_curve(wave, errs):
+    """
+    Calculates the white light curve for a given spectrum. 
+    This reduces the spectrum to a single point. Useful as a
+    comparison between the error binning in Pandexo and in my code.
+    
+    ================== Params ==================
+    
+    wave   : array or list of wavelengths (microns)
+    errs   : array or list of errors on transit spectrum (ppm)
+    
+    ============================================
+    """
+    
+    # loops over entire error array
+    weights      = 1 / (errs ** 2)
+    standard_err = np.sqrt(1 / np.sum(weights))
+    
+    # bin center at the center of wavelength array
+    bin_center = np.mean([wave[0],wave[-1]])
+    bin_width  = wave[-1] - wave[0]
+    
+    return bin_center, standard_err, bin_width
+    
+    
 def calc_bin_widths(wave, R):
     """
     Calculates deltalambda of a wavelength array, given the resolution:
@@ -47,6 +72,8 @@ def bin_errs(wave, errs, R):
     bin_widths  = [] # full bin widths
     
     # treating first bin a bit different
+    bin_center   = wave[0] * (2*R + 1)/(2*R - 1)
+    
     dellamb0     = wave[0] / R # bin center is 0th element
     bin_indices0 = np.where(wave <= wave[0] + dellamb0/2)
     
@@ -60,12 +87,8 @@ def bin_errs(wave, errs, R):
     bin_widths.append(dellamb0)
     
     # calculate next bin center
-    bin_center = (2*R*wave[bin_indices0[0][-1] + 1]) / (2*R - 1)
-    """print('bin min ', 'na')
-    print('bin center ', wave[0])
-    print('bin max ', wave[wave <= wave[0] + dellamb0/2][-1])
-    print('next bin min ', wave[bin_indices0[0][-1] + 1])
-    print("---------------------------------------")"""
+    bin_center = wave[0] * (2*R + 1)/(2*R - 1)
+    
     # repeat calculation above until last bin
     while bin_center <= wave[-1]:
         
@@ -73,27 +96,17 @@ def bin_errs(wave, errs, R):
         bin_indices  = np.where(np.logical_and(wave >= bin_center - dellamb/2, wave <= bin_center + dellamb/2))
         weights      = 1 / (errs[bin_indices] ** 2)
         standard_err = np.sqrt(1 / np.sum(weights))
-        """print('bin min ', wave[wave >= bin_center - dellamb/2][0])
-        print('bin center ', bin_center)
-        print('bin max ', wave[wave <= bin_center + dellamb/2][-1])
-        print('next bin min ', wave[bin_indices[0][-1] + 1])
-        print("---------------------------------------")"""
+        
         new_errs.append(standard_err)
         bin_centers.append(bin_center)
         bin_widths.append(dellamb)
-        #print(bin_indices)
-        """print('bin center: ', bin_center)
-        print('bin width: ', dellamb)
-        print('bin min: ', bin_center - dellamb/2)
-        print('bin max: ', bin_center + dellamb/2)
-        print('--------------------------------------')"""
         
         # calculate next bin center
-        bin_center = (2*R*wave[bin_indices[0][-1] + 1]) / (2*R - 1)
+        bin_center = bin_center * (2*R + 1)/(2*R - 1)
         
     # for last bin, reset bin center to last element of wavelength array
     # bin_width is now just the rest of the wavelength array
-    bin_center   = wave[-1]
+    """bin_center   = wave[-1]
     bin_indices  = np.where(wave >= bin_centers[-1] + bin_widths[-1]/2)
     dellamb      = 2 * (bin_center - wave[bin_indices[0][0]])
     weights      = 1 / (errs[bin_indices] ** 2)
@@ -101,7 +114,7 @@ def bin_errs(wave, errs, R):
 
     new_errs.append(standard_err)
     bin_centers.append(bin_center)
-    bin_widths.append(dellamb)
+    bin_widths.append(dellamb)"""
 
     
     return new_errs, bin_centers, bin_widths
